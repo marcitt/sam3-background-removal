3af271d0ea80f6770d87acafcf29cbdf70ea8eeb
Marci <marcitalbot23@gmail.com>
2026-07-23 14:22:06 +0100

    modified to imageio_ffmpeg

--- src/video_decode.py
+++ src/video_decode.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import argparse
+import imageio_ffmpeg
 
 PROJECT_ROOT = os.environ.get(
     "SAM3_PROJECT_ROOT",
@@ -29,8 +30,9 @@ MASK_DIR = os.path.join(OUTPUT_DIR, "masks")
 os.makedirs(FRAME_DIR, exist_ok=True)
 os.makedirs(MASK_DIR, exist_ok=True)
 
+ffmpeg_path = imageio_ffmpeg.get_ffmpeg_exe()
 subprocess.run([
-    "ffmpeg",
+    "ffmpeg_path",
     "-i", VIDEO_PATH,
     "-vsync", "0",
     os.path.join(FRAME_DIR, "frame_%06d.png"),
